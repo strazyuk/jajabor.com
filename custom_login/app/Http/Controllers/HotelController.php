b70c0914b9f0656f1e1f5bb9cd53fdcb0d31a66d
malihashawana <shawana.maliha@g.bracu.ac.bd>
2024-12-08 21:11:02 +0600

    hi1

--- custom_login/app/Http/Controllers/HotelController.php
+++ custom_login/app/Http/Controllers/HotelController.php
@@ -3,17 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Hotel; // Import the Hotel model
+use App\Models\Hotel; 
 use App\Models\Favorite;
 
 class HotelController extends Controller
 {
     public function index()
     {
-        // Retrieve all hotels
+
         $hotels = Hotel::all();
 
-        // Retrieve favorite hotels for the authenticated user
         $favoriteHotels = Favorite::where('user_id', auth()->id())->pluck('hotel_id')->toArray();
 
         return view('hotels.index', compact('hotels', 'favoriteHotels'));
@@ -23,18 +22,17 @@ class HotelController extends Controller
     {
         $query = Hotel::query();
 
-        // Check if the search input is filled
+
         if ($request->filled('search')) {
             $search = $request->search;
 
-            // Search by name OR location
             $query->where('name', 'like', '%' . $search . '%')
                   ->orWhere('location', 'like', '%' . $search . '%');
         }
 
         $hotels = $query->get();
 
-        // Retrieve favorite hotels for the authenticated user
+
         $favoriteHotels = Favorite::where('user_id', auth()->id())->pluck('hotel_id')->toArray();
 
         return view('hotels.index', compact('hotels', 'favoriteHotels'));

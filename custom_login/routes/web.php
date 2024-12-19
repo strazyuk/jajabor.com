4bfb21637b8602ff9855b42b6e943a9f24c9ec99
strazyuk <129723342+strazyuk@users.noreply.github.com>
2024-12-19 22:25:55 +0600

    web.php updated

--- custom_login/routes/web.php
+++ custom_login/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\FlightController;
 use App\Http\Controllers\HotelController;
 use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\FavoriteController;
+use app\Http\Controllers\HomeController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\LocationController;
 
@@ -86,7 +87,7 @@ Route::prefix('hotels')->group(function () {
 
 // Location Routes
 Route::get('/locations', [LocationController::class, 'index'])->name('locations.index');
-
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //weather
 Route::get('weather', [WeatherController::class, 'showWeather'])->name('weather.show');
